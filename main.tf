a84612ad287d31867cb80a7e3141c07f32e82c66
jingwei15 <jingwei.tan@ncs.com.sg>
2021-05-05 16:20:53 +0800

    Point backend to correct TFC org and workspace

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ terraform {
   required_version = "~> 0.14"
 
   backend "remote" {
-    organization = "OrgTest"
+    organization = "Week3Test"
 
     workspaces {
       name = "gh-actions-demo"
